c94f3214b39036659ad559a76b91e99eac7214f9
Mahmut Ekrem Acar <mahmutekremacar@gmail.com>
2023-10-26 21:17:16 +0200

    .

---
+++ blatt2.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(define (ungerade-end x)
+  (- (* 2 x ) 1 ))
+
+(define summe 1)
+(define (nat-wuerzel  x)
+  (if ( or (< (ungerade x) 1) (= (ungerade x) 1) )))
+      
+      
+
+(ungerade-end 6)
+
