f35666713777a269978e0885ab884695d8cf7863
Mahmut Ekrem Acar <mahmutekremacar@gmail.com>
2023-10-27 16:53:48 +0200

    .

--- racket_yt_l4_case_splitting.rkt
+++ racket_yt_l4_case_splitting.rkt
@@ -46,5 +46,57 @@ fire.
 ;  Empty list == '()
 
 #|
- L4 min: 13 left
+Every list in racket is built out by "cons cells"
+
+cons cell is a pair. It has a first element and the second element (cons 0 1)
+
+Use "car" to get the element in the left side (car (cons 0 1)) == 0
+Use "cdr" to get the element in the right side (cdr (cons 0 1)) == 1
+
 |#
+
+#|
+Lists
+
+Racket has lists-- sequences of cons cells ending w/'()
+The empty lis( or "null") is special '()
+Many ways to build them
+  (list 1 2 3) ;; Variadic function
+  '( 1 2 3)    ;; Datum represention
+
+There are three operations on lists
+  empty?/ null?
+  first/car
+  rest/cdr
+|#
+
+#;( define one-to-three (list 1 2 3))
+; Above and below gives the same result
+(define one-to-three '( 1 2 3))
+
+
+(cdr (cdr one-to-three)) ;; 3
+(null? one-to-three)     ;; #f
+(car (cdr one-to-three)) ;; 2
+
+(define one-to-five '( 1 2 3 4 5))
+(define (parser x )(car x) + (is-five (cdr x)))
+
+(define (is-five x) (if (= (car x) 5)
+                        #t
+                    (parser x) ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
