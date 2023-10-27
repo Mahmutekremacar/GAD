b1cdb90c96773efd17ac42f6a8ee1c3815c3ef0d
Mahmut Ekrem Acar <mahmutekremacar@gmail.com>
2023-10-27 17:21:43 +0200

    erledigt

--- racket_yt_l4_case_splitting.rkt
+++ racket_yt_l4_case_splitting.rkt
@@ -27,7 +27,7 @@ fire.
   +
   (println "what's up")
   +
-  (println name))
+  (println name)) 
 
 ;; how to write cond only using if?
 #;(define (abs x)
@@ -79,16 +79,65 @@ There are three operations on lists
 (null? one-to-three)     ;; #f
 (car (cdr one-to-three)) ;; 2
 
-(define one-to-five '( 1 2 3 4 5))
+(define lst '( 1 2 3 4 5))
+
 (define (parser x )(car x) + (is-five (cdr x)))
 
 (define (is-five x) (if (= (car x) 5)
                         #t
                     (parser x) ))
+(define a-to-be '("a" "b" "c"))
 
+#|
+List continued
 
+Using empty?, car, cdr, we can write many utilities
+  All definable ourselves, also in Racket by default
+  (length l)    -- Length of l
+  (list-ref l i)-- Get the ith element of list(0-indexed)
+  (append l0 l1)-- Append l1 to the end of the l0
+  (reverse l)   -- Reverse the list
+  (member x l)  -- Check if x is in l
 
+> (length lst)
+5
+>(list-ref lst 0)
+1
+> (append lst '(6 7 8))
+'(1 2 3 4 5 6 7 8)
+> (reverse lst)
+'(5 4 3 2 1)
+> (member 5 lst)
+'(5)
+> (member 0 lst)
+#f
+|#
 
+#|
+Using cond, write a function that takes a list l and an index x and returns...
+
+  The first element if x= 0
+  The second element if x=1
+  ...
+  Otherwise unknown
+|#
+(define (foo x y)
+  (cond [(= y 0)(list-ref x y)]
+        [(= y 1)(list-ref x y)]
+        [(= y 2)(list-ref x y)]
+        [else (println "Unknown")]))
+
+#;(foo '(1 2 3 4) 0)
+#;(foo '(1 2 3 4) 1)
+#;(foo '(1 2 3 4) 2)
+#;(foo '(1 2 3 4) 3)
+ 
+(define (index-returner x y )
+  (if (member y x) (list-ref x y) (println "Unknown")))
+
+#;(index-returner '(1 2 3 4) 3)
+
+  
 
 
 
