c9ee97b49344a49198019675006624999b424d1d
Mahmut Ekrem Acar <mahmutekremacar@gmail.com>
2023-10-27 18:33:36 +0200

    L5 erledigt

--- racket_yt_l5_recursion_over_lists.rkt
+++ racket_yt_l5_recursion_over_lists.rkt
@@ -1,1 +1,15 @@
 #lang racket
+(define x '(42 -1 3 1 7))
+
+(define (only-positives l)
+  (if (empty? l)
+      ;; handle the base case
+      '()
+      ;; otherwise, it's a cons cell
+      (if (<= (car l) 0)
+          ;;eclude it
+          (only-positives (cdr l))
+          ;; iclude it
+          (cons (car l) (only-positives (cdr l))))))
+
+(only-positives x)
