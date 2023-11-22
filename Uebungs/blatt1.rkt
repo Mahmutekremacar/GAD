3fd7084aa6e67e86b2937a7125ced5743d89f500
Mahmutekremacar <mahmutekremacar@gmail.com>
2023-11-22 21:11:05 +0100

    Reorganized blatt exercises to a folder.

---
+++ Uebungs/blatt1.rkt
@@ -0,0 +1,60 @@
+#lang racket
+
+(define aufgabe1x
+  (- (/ (+ 9 6) (*(- 3 1) 5)) (* (- (/ 7 8) 2) 4)))
+aufgabe1x
+
+(define (g u v w)
+  (+ (/ (- v (* 7 u)) (- u w ) (/ (+ u v) (- (* w 6) v )))))
+(println "--------------")
+(g 1 2 3)
+(g 3 11 2)
+
+
+(define (my-max x y)
+ (if (> x y)
+     x
+     y)) 
+
+(println "--------------")
+(my-max 5 2)
+(my-max 10 23)
+(my-max 4 4)
+
+(define (groesser-zehn? x)
+  (if (> x 10)
+      #t
+      #f))
+(println "--------------")
+(groesser-zehn? 4)
+(groesser-zehn? 10)
+(groesser-zehn? 15)
+
+(define (groesserp? x y z)
+  (if (> (+ x y ) z )
+      #t
+      #f))
+(println "--------------")
+
+(groesserp? 4 5 6)
+(groesserp? 2 12 10)
+(groesserp? 3 3 6)
+
+
+(define (squared-max x y z)
+  (define (quadrat b)
+    (* b b))
+  (max ( quadrat x ) (quadrat  y ) (quadrat  z) ))
+
+(println "--------------")
+(squared-max 10 21 -11)
+(squared-max 25 26 10)
+
+
+(define (max3 x y z)
+(my-max (my-max x y) z))
+
+
+(println "--------------")
+(max3 17 22 16)
+
---
+++ Uebungs/blatt3_1.rkt
@@ -0,0 +1,57 @@
+#lang racket
+;;Aufgabe 1
+
+#;(define (sin-approx x)
+  (define (sonst x) (lambda (x) (- (* 3 (sin-approx (/ x 3))) (* 4 ( expt((sin-approx (/ x 3)) 3))))))
+  (if (> x 0.1) (sonst x) x ))
+
+(define (sinus-approx x )
+  (if (<= x 0.1) 
+      x
+      (- (* 3.0 (sinus-approx (/ x 3.0))) (* 4.0 (expt (sinus-approx (/ x 3.0)) 3.0)))))
+(println ",,,,")
+
+(sinus-approx 750)
+;;erl
+
+;;Aufgabe 2
+
+(define (count-perm x)
+  (define (count-perm-iter x total)
+    (if (> x 0) (count-perm-iter (- x 1) (* total x)) total))
+  (count-perm-iter x 1))
+
+(newline)
+(count-perm 2)
+(count-perm 3)
+(count-perm 5)
+(newline)
+;erl
+
+;;Aufgabe 3
+
+(define (isbn-test isbn)
+  (define (iter isbn digit total)
+    (define (digitTimesAndDim digit total)
+      (iter (quotient isbn 10) (- digit 1) (+ total (* (remainder isbn 10)digit))))
+    (if (>= digit 0) (digitTimesAndDim digit total) total))
+  (if (= (remainder (iter isbn 9 0) 11) 10 ) "X" (remainder (iter isbn 9 0) 11))) 
+
+(println "---------")
+(isbn-test 344615497)
+(isbn-test 026201153) 
+(isbn-test 392511825)
+;erl
+
+;;Aufgabe 4
+
+(define (zylinder-kegel radius-zylinder hoehe-zylinder radius-kegel hoehe-kegel)
+  (define pi  3.1415926535897932384626433832795 )
+  (/(* pi (expt radius-zylinder 2) hoehe-zylinder )
+    (/(* pi (expt radius-kegel 2) hoehe-kegel) 3)))
+(println "---------")
+
+(zylinder-kegel 3 5 3 5)
+;erl
+
+;;Aufgabe 5
