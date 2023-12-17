6abe26c947ba7ad90538da71ef4e5952263fdd5e
Mahmutekremacar <mahmutekremacar@gmail.com>
2023-12-17 17:47:47 +0100

    blat 6

--- Uebungs/blatt6.rkt
+++ Uebungs/blatt6.rkt
@@ -31,7 +31,8 @@ Falls es keine n-te Zahl gibt, die diese Forderung
 ;;erl
 
 
-;;Aufgabe 2
+(display "Aufgabe 2")
+(newline)
 #| (write a digit finder also)
 Start by writing a function that changes the last digit
 to first than write a scond func that controls if the changed num
@@ -57,12 +58,110 @@ bigger or equal the original one (if not ) add +10
 (gleiche-ziffern 120)
 
 ;;erl
-
+(newline)
+(display "Aufgabe 3")
 
 (define (konst-addierer n )
-  (lambda (x n) (+ x n)))
+  (lambda (x) (+ x n)))
+
+(newline)
+(define plus1 (konst-addierer 1))
+  (plus1 98)
+(define plus10 (konst-addierer 10))
+(plus10 98)
+
+
+
+
 
 #;(define plus1 (konst-addierer 1)
-  (plus1 98))
+  (plus1 98)) 
+#;(newline)
+#;(display "TEST")
+#;(define test
+  (lambda (x)
+  (lambda (y)
+    (+ x y))))
+#;(newline)
+#;(define add27
+  (test 27))
 
+#;(add27 2)
+
+#;(define ( test2 x)
+  (lambda (y)
+    (if (>= y 10) (+ x y) (- x 10))))
+#;(newline)
+#;(define add10
+  (test2 10))
+#;(add10 200)
+
+(display "Aufgabe 4")
+
+(define (konst-ggt b)
+  (define (ggt a b )
+  (if (= b 0)
+      a
+      (ggt b (remainder a b)))
+  )
+  
+  (lambda (a) (ggt a b))
+  )
+(newline)
+
+(define ggt10 (konst-ggt 10))
+(define ggt987 (konst-ggt 987))
+
+(ggt10 25)
+(ggt10 27)
+
+(ggt987 762351)
+(ggt987 98123746)
+
+(newline)
+(display "Aufgabe5")
+
+#;(define (paar-operation op))
+
+(define (paar-operation op)
+  (lambda (x) (op (car x) (cdr x)))
+  )
+
+(newline)
+(define paar=? (paar-operation =))
+(paar=? (cons 2 3)) 
+(paar=? (cons 3 3))
+
+(define paar<? (paar-operation <))
+(paar<? (cons 2 3)) 
+(paar<? (cons 3 3))
+
+(define paar+ (paar-operation +))
+(paar+ (cons 2 3)) 
+(paar+ (cons 3 3))
+
+(newline)
+(display "Aufgabe 6")
+(newline)
+; https://docs.racket-lang.org/sdraw/
+(require sdraw)
+(sdraw (list 1 2))
+(sdraw (list 1 2) #:null-style '/)
+
+(newline)
+(display "Struktur1")
+(define struktur1 (cons (cons 24 (cons 1 2)) (cons 20 (cons 1 1))))
+(sdraw struktur1 #:null-style '/ )
+
+(display "Struktur2")
+(define struktur2 (cons ( cons 7 (cons 2 (cons 3 (cons 1 null)))) null))
+(sdraw struktur2 #:null-style '/ )
+
+(display "Struktur 3")
+(define struktur3 (cons (cons 3 (cons 3 null)) (cons 3 (cons 3 (cons 3 3)))))
+(sdraw struktur3 #:null-style '/)
+
+(display "Struktur 4")
+(define struktur4 (cons (cons 10 (cons 7 (cons 8 (cons 9 (cons 3 null))))) (cons (cons null (cons 6 (cons 1 (cons 0 (cons 2 4)))))(cons 3 3))))
+(sdraw struktur4 #:null-style'/)
 
