4408dabae22bfb601bae57415f681aa6ff2eab5f
Mahmutekremacar <mahmutekremacar@gmail.com>
2024-01-13 13:29:34 +0100

    .

--- Uebungs/blatt10.rkt
+++ Uebungs/blatt10.rkt
@@ -1,1 +1,27 @@
 #lang racket
+;;Aufgabe 1
+#;(define (removeFirstLast string)
+  (define (removeIter string lst)
+    (if (= string))))
+
+
+(define (removeFirstLast string)
+  (define n (string-length string))
+  (substring string 1 (- n 1)))
+
+(removeFirstLast "Hallo Welt")
+(removeFirstLast "Algorithmik")
+
+;;er
+
+(define (sicheresPasswort passwort)
+  (define (spec-char lstpass num) 
+    (if (char-alphabetic? (first lstpass)) (spec-char (rest lstpass) num ) (spec-char (rest lstpass) (+ num 1))))
+  (define (passIter passwort lstpass)
+    (if (>= (length lstpass) 8 )
+        (if (>= (spec-char (string->list passwort) 0) 2) #t #f) #f))
+  (passIter passwort (string->list passwort)))
+
+(sicheresPasswort "aUljsb!f/KasDhf")
+
+;nicht erl
