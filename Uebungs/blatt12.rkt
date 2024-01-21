3bc509fc777cb3e38043e41d6a4002b9cd4b54f8
Mahmutekremacar <mahmutekremacar@gmail.com>
2024-01-21 16:52:29 +0100

    Blatt 12 4~~

--- Uebungs/blatt12.rkt
+++ Uebungs/blatt12.rkt
@@ -22,3 +22,62 @@
   )
 
 (wort-sortieren "Guten Tag")
+;;Aufgabe2
+
+(define (string-enthalten satz wort)
+ (define (string-enthalten-iter satz wort satz-length wort-length)
+  (if (< satz-length wort-length)
+      #f
+      (if (equal? wort (substring satz 0 wort-length))
+          #t
+          (string-enthalten-iter (substring satz 1) wort (- satz-length 1) wort-length)
+          )
+      )
+  )
+  (string-enthalten-iter satz wort (length (string->list satz)) (length (string->list wort)))
+)
+(string-enthalten "Heute ist Dienstag" "Dienstag")
+
+;;Aufgabe 3
+
+(define (tokenizer satz token)
+  (define (tokenizer-iter satz token temp liste)
+    (if (empty? satz)
+      (if (empty? temp)
+        liste
+        (append liste (list (list->string temp)))
+        )
+      (if (char=? (car satz) (car token))
+        (tokenizer-iter (cdr satz) token '() (append liste (list (list->string temp))))
+        (tokenizer-iter (cdr satz) token (append temp (list (car satz))) liste)
+        )
+      )
+    )
+  (tokenizer-iter (string->list satz) (string->list token) '() '())
+  )
+
+(tokenizer "Happy new year!" "e")
+
+;;Aufgabe 4
+
+(define (vector-add vector1 vector2)
+  
+  (define (wie-lang v laenge)
+    (cond
+      [(empty? (cdr v)) (+ 1 laenge)]
+      [else (wie-lang (cdr v) (+ laenge 1))]))
+  
+  (define (adder v zeros)
+    (if (= 0 zeros)
+        v
+        (adder (vector-append v #(0)) (- zero 1))))
+
+  (define (vector-add-iter vector1 vector2 sum v1lang v2lang  )
+    (if (> v1lang v2lang)
+        
+        )
+    
+    )
+
+  )
+#;(wie-lang (vector->list vector1)) #;(wie-lang (vector->list vector2))
